292edcdf9a6ca05221fe3acd1e3ba78a8cf18fa0
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-06-25 02:44:54 +0100

    l

--- app/Util/hash.py
+++ app/Util/hash.py
@@ -6,5 +6,5 @@ password_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
 class Hash():
     def hash_password(password):
         return password_context.hash(password)
-    def verify_password(plain_password,hashed_password):
+    def verify_password(self,plain_password,hashed_password):
         return password_context.verify(plain_password,hashed_password)
