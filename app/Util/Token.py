9f72124c3c302b2a43a12f039711043f9edcdb17
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-06-25 01:27:43 +0100

    fixed an error

--- app/Util/Token.py
+++ app/Util/Token.py
@@ -7,9 +7,9 @@ def create_access_token(data:dict,expiry:Optional[timedelta]=None):
     to_encode=data.copy()
     
     if(expiry):
-        expiry_date=datetime.utcnow + expiry
+        expiry_date=datetime.utcnow() + expiry
     else:
-        expiry_date=datetime.utcnow + timedelta(hours=1)
+        expiry_date=datetime.utcnow() + timedelta(hours=1)
     to_encode.update({"exp":expiry_date})
     encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=Algorithm)
     return encoded_jwt
