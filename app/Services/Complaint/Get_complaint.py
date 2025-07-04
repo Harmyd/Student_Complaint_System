b913eee027c373916fdddb92c67d7ad60bb714e3
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-07-04 20:39:20 +0100

    added get complaint functionality

--- app/Services/Complaint/Get_complaint.py
+++ app/Services/Complaint/Get_complaint.py
@@ -2,19 +2,29 @@ from ...databases import Session
 from ...models import Complains,Students
 from fastapi.responses import JSONResponse
 from fastapi import status
+from fastapi.encoders import jsonable_encoder
 
 def get_user_complaint(db:Session,current_user):
     user_id=current_user['user_id']
 
-    user=db.query(Students).filter(Students.id==user_id)
+    user=db.query(Students).filter(Students.id==user_id).first()
     if not user:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
             content={"message":"user not found"}
         )
-    Complaint=db.query(Complains).filter(Complains.student_id==user_id).all()
+    complaint=db.query(Complains).filter(Complains.student_id==user_id).all()
+    if not complaint:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message":"No complaint found for this user"}
+        )
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"message":"Successfull",
+                 "Complaints":jsonable_encoder(complaint)}
+    )
   
-    pass
 
 def get_specific():
     pass
