c99312673d0922b96fdc0faec09d36e4a73d1073
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-07-30 14:19:31 +0100

    Resolved coroutine error by adding missing await

--- app/Routes/User.py
+++ app/Routes/User.py
@@ -3,6 +3,7 @@ from ..databases import Session,get_db
 from ..Util.Oauth import get_token
 from ..Services.User import upload_profile_image,get_user_details
 from ..Services.User.edit_user import edit_user
+from ..Services.User.get_user_details import get_user_detail
 from ..schema import UserOut
 from typing import Optional
 
@@ -20,8 +21,8 @@ def Upload_picture(
     return upload_profile_image.upload_profile_image(Picture,db,user)
 
 @USER.get("/get_user_detail",status_code=status.HTTP_200_OK,response_model=UserOut)
-def user_info(db:Session=Depends(get_db),user=Depends(get_token)):
-    return get_user_details.get_user_detail(db,user)
+async def user_info(db:Session=Depends(get_db),user=Depends(get_token)):
+    return await get_user_detail(db,user)
 
 @USER.post("/edit_user_detail",status_code=status.HTTP_200_OK)
 async def edit_user_detail(
--- app/Routes/auth.py
+++ app/Routes/auth.py
@@ -1,14 +1,15 @@
 from ..databases import get_db,Session
 from fastapi import Depends,APIRouter,status
 from ..schema import UserSignUp,Login
-from ..Services import signup,login
+from ..Services.signup import SignUp
+from ..Services.login import login
 
 Auth= APIRouter(prefix="/auth",tags=["auth"])
 
 @Auth.post("/signup",status_code=status.HTTP_201_CREATED)
-def Signup(request:UserSignUp,db:Session=Depends(get_db)):
-    return signup.SignUp(request,db)
+async def Signup(request:UserSignUp,db:Session=Depends(get_db)):
+    return await SignUp(request,db)
 
 @Auth.post("/login",status_code=status.HTTP_200_OK)
-def Login(request:Login,db:Session=Depends(get_db)):
-    return login.login(request,db)
+async def Login(request:Login,db:Session=Depends(get_db)):
+    return await login(request,db)
